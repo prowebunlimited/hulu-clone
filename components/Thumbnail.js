be98de1447fdfee785fa88ef6a20610fdf9b3f42
Aravinda Guruprasad <aguruprasad@uei.com>
2021-06-15 23:03:23 +0530

    Added padding

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -12,8 +12,10 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'
     >
       <Image layout='responsive' src={imagePath} height={1080} width={1920} />
-      <div className='p-2 sm:pl-10'>
-        <p className='truncate max-w-md'>{result.overview}</p>
+      <div className='p-2 pl-10 sm:pl-2'>
+        <p className='truncate break-words max-w-xs sm:max-w-md'>
+          {result.overview}
+        </p>
         <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
           {result.title || result.original_name}
         </h2>
