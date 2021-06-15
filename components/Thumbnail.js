e1d9041ece8ac233fe719cd7b708d2adf21fa87b
Aravinda Guruprasad <aguruprasad@uei.com>
2021-06-15 22:52:53 +0530

    Added padding for sm title

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -12,12 +12,12 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'
     >
       <Image layout='responsive' src={imagePath} height={1080} width={1920} />
-      <div className='p-2'>
-        <p className='truncate max-w-md sm:px-20'>{result.overview}</p>
-        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold sm:px-20'>
+      <div className='p-2 sm:pl-10'>
+        <p className='truncate max-w-md'>{result.overview}</p>
+        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
           {result.title || result.original_name}
         </h2>
-        <p className='flex items-center opacity-0 group-hover:opacity-100 sm:px-20'>
+        <p className='flex items-center opacity-0 group-hover:opacity-100'>
           {result.media_type && `${result.media_type} *`}{' '}
           {result.release_date || result.first_air_date} *{' '}
           <ThumbUpIcon className='h-5 mx-2' />
